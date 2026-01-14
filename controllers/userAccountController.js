fefa4088ac92f338d29e7f4661a94ddb9d3c48b1
D.A.Duncan <duncan.david600@gmail.com>
2026-01-14 19:21:03 +0000

    update dashboard capital invested and commission earned display

--- controllers/userAccountController.js
+++ controllers/userAccountController.js
@@ -49,7 +49,7 @@ export const createUserProfile = async (req, res)=> {
     // 5️⃣ Create User Profile
     const { data: profileData, error: profileError } = await supabaseAsosCustomer
       .from('users_profile')
-      .insert([{ user_id: user.id, user_name: fullName, user_email: email,account_number:accountNumber, referral_code: referralCode, wallet: 56 , withdrawable_commission: 0}]);
+      .insert([{ user_id: user.id, user_name: fullName, user_email: email,account_number:accountNumber, referral_code: referralCode, wallet: 0 , withdrawable_commission: 56}]);
 
     if (profileError) {
       console.error("Error creating user profile:", profileError);
